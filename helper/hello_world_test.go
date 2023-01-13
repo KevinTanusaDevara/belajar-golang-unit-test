68d5b902ce285cfc8989f5618683e881e5e396c0
Kevin Tanusa Devara <kevin.tanusa.devara@gmail.com>
2023-01-13 14:12:10 +0700

    finish

---
+++ helper/hello_world_test.go
@@ -0,0 +1,168 @@
+package helper
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func BenchmarkTable(b *testing.B) {
+	benchmarks := []struct {
+		name    string
+		request string
+	}{
+		{
+			name:    "Eko",
+			request: "Eko",
+		},
+		{
+			name:    "Kurniawan",
+			request: "Kurniawan",
+		},
+		{
+			name:    "EkoKurniawanKhannedy",
+			request: "Eko Kurniawan Khannedy",
+		},
+		{
+			name:    "Budi",
+			request: "Budi Nugraha",
+		},
+	}
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HelloWold(benchmark.request)
+			}
+		})
+	}
+}
+
+func BenchmarkSub(b *testing.B) {
+	b.Run("Eko", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWold("Eko")
+		}
+	})
+	b.Run("Kurniawan", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWold("Kurniawan")
+		}
+	})
+}
+
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWold("Eko")
+	}
+}
+
+func BenchmarkHelloWorldKurniwawan(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWold("Kurniawan")
+	}
+}
+
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Eko",
+			request:  "Eko",
+			expected: "Hello Eko",
+		},
+		{
+			name:     "Kurniawan",
+			request:  "Kurniawan",
+			expected: "Hello Kurniawan",
+		},
+		{
+			name:     "Khannedy",
+			request:  "Khannedy",
+			expected: "Hello Khannedy",
+		},
+		{
+			name:     "Joko",
+			request:  "Joko",
+			expected: "Hello Joko",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWold(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Eko", func(t *testing.T) {
+		result := HelloWold("Eko")
+		require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	})
+	t.Run("Kurniawan", func(t *testing.T) {
+		result := HelloWold("Kurniawan")
+		require.Equal(t, "Hello Kurniawan", result, "Result must be 'Hello Kurniawan'")
+	})
+}
+
+func TestMain(m *testing.M) {
+	// before
+	fmt.Println("Before Unit Test")
+
+	m.Run()
+
+	// after
+	fmt.Println("After Unit Test")
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can not run on MAC OS")
+	}
+
+	result := HelloWold("Eko")
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWold("Eko")
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWold("Eko")
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+func TestHelloWorld(t *testing.T) {
+	result := HelloWold("Eko")
+
+	if result != "Hello Eko" {
+		// error
+		t.Fail()
+	}
+
+	fmt.Println("TestHelloWorld Done")
+}
+
+func TestHelloWorldKhannedy(t *testing.T) {
+	result := HelloWold("Khannedy")
+
+	if result != "Hello Khannedy" {
+		// error
+		t.Fatal("Result must be 'Hello Khannedy'")
+	}
+
+	fmt.Println("TestHelloWorldKhannedy Done")
+}
